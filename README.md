b2f9efd1f6a0824c0da97f9fe8b37009057c1812
Lucas Boscaini <lucasboscaini@gmail.com>
2016-03-10 01:21:34 -0300

    Add structure

---
+++ README.md
@@ -0,0 +1,26 @@
+No fancy generators, just clone this repo and build your app on top of it.
+
+### Package
+
+- Uses AngularJS, Gulp, Jade, Sass (with sourcemaps), browserSync.
+- Respects [angularjs-styleguide](https://github.com/johnpapa/angularjs-styleguide).
+
+### Update dependencies
+
+To update dependencies in package.json:
+
+`$ npm install -g npm-check-updates`
+`$ ncu -u`
+
+### Setup
+
+- `$ git clone https://github.com/lucasb/jags`
+- Find and replace `jags` to `yourappname` in `./`
+- `$ npm install`
+- `$ npm install -g gulp`
+- `$ gem install sass`s
+- `$ gulp`
+
+### Deployment
+
+Run `$ gulp dist` for the production ready code in `build`.
---
+++ app/scripts/entry.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var appDependencies = [
+  'ng',
+  'ui.router'
+];
+
+angular
+  .module('app', appDependencies)
+  .config(appConfig)
+  //.constant('config', require('../../config.json'));
+
+require('./app.controller');
+require('./about.controller');
+
+appConfig.$inject = ['$stateProvider', '$urlRouterProvider', '$locationProvider'];
+
+function appConfig ($stateProvider, $urlRouterProvider, $locationProvider) {
+  var routes = [
+    {
+      name: 'main',
+      path: ''
+    },
+    {
+      name: 'about',
+      path: 'about'
+    }
+  ];
+
+  routes.forEach(function(route){
+    $stateProvider.state(route.name, {
+      url: '/' + route.path,
+      views: {
+        guest: { templateUrl: 'views/' + route.name + '.html' }
+      }
+    });
+  });
+
+  $urlRouterProvider.otherwise("/404");
+  $locationProvider.html5Mode(true);
+}
